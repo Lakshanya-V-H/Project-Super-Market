3e55aa62825c23e555a495722c3258817cb5bb6b
Lakshanya-V-H <125448285+Lakshanya-V-H@users.noreply.github.com>
2024-01-05 10:36:39 +0530

    Footer.jsx - linking updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -21,17 +21,17 @@ class Footer extends React.Component{
                                 <h3>extra links</h3>
                                 {/*<Link to={`#`}> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> my order </Link>
                                 <Link to={"#"}> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> my account </Link>*/}
-                                <Link href="/FAQ"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> FAQs </Link>
-                                <Link href="/Privacy"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> privacy policy </Link>
-                                <Link href="/Terms"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> terms & conditions </Link>
+                                <Link to={`/FAQ`}> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> FAQs </Link>
+                                <Link to={`/Privacy`}> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> privacy policy </Link>
+                                <Link to={`/Terms`}> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> terms & conditions </Link>
                             </div>
                             <div class="box">
                                 <h3>follow us</h3>
-                                <Link href='https://www.facebook.com/'> <i class="fab fa-facebook-f"><FontAwesomeIcon icon={faFacebookF} /></i> facebook </Link>
-                                <Link href='https://twitter.com/?lang=en'> <i class="fab fa-twitter"><FontAwesomeIcon icon={faTwitter} /> </i>twitter </Link>
-                                <Link href='https://www.instagram.com/'> <i class="fab fa-instagram"><FontAwesomeIcon icon={faInstagram} /></i> instagram </Link>
-                                <Link href='https://www.linkedin.com/'> <i class="fab fa-linkedin"><FontAwesomeIcon icon={faLinkedin} /></i> linkedin </Link>
-                                <Link href='https://www.pinterest.com/'> <i class="fab fa-pinterest"><FontAwesomeIcon icon={faPinterest} /></i> pinterest </Link>
+                                <Link to={`https://www.facebook.com/`}> <i class="fab fa-facebook-f"><FontAwesomeIcon icon={faFacebookF} /></i> facebook </Link>
+                                <Link to={`https://twitter.com/?lang=en`}> <i class="fab fa-twitter"><FontAwesomeIcon icon={faTwitter} /> </i>twitter </Link>
+                                <Link to={`https://www.instagram.com/`}> <i class="fab fa-instagram"><FontAwesomeIcon icon={faInstagram} /></i> instagram </Link>
+                                <Link to={`https://www.linkedin.com/`}> <i class="fab fa-linkedin"><FontAwesomeIcon icon={faLinkedin} /></i> linkedin </Link>
+                                <Link to={`https://www.pinterest.com/`}> <i class="fab fa-pinterest"><FontAwesomeIcon icon={faPinterest} /></i> pinterest </Link>
                             </div>
                             <div class="box">
                                 <h3>newsletter</h3>
